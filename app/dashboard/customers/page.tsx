b55e80f23025da244d771a9cf1340fe2d677f7ad
Martin <mselvenis@gmail.com>
2024-12-27 16:14:22 +0000

    configuration

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,15 +1,9 @@
-import {
-  Table,
-  TableBody,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { getCustomers } from "@/data-access/customer";
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
+import {Table, TableBody, TableHead, TableHeader, TableRow,} from "@/components/ui/table";
+import {getCustomers} from "@/data-access/customer";
+import {auth} from "@/lib/auth";
+import {headers} from "next/headers";
 import Link from "next/link";
-import { redirect } from "next/navigation";
+import {redirect} from "next/navigation";
 import CustomerRow from "./CustomerRow";
 
 export default async function CustomersPage() {
