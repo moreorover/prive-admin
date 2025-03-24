c1be05046f186b5e3846ee807d3035d3feeff341
Martin <mselvenis@gmail.com>
2025-03-24 12:45:25 +0000

    fix: hair order migrations (#36)

--- prisma/migrations/20250303121854_transaction_status/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateEnum
-CREATE TYPE "TransactionStatus" AS ENUM ('PENDING', 'COMPLETED');
-
--- AlterTable
-ALTER TABLE "transactions" ADD COLUMN     "completedDateBy" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "status" "TransactionStatus" NOT NULL DEFAULT 'PENDING';
