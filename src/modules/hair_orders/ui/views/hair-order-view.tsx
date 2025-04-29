e1020ca291cfb84f5fbbebb88b237928852a0c00
Martin <mselvenis@gmail.com>
2025-04-29 18:10:16 +0100

    feature: hair price

--- src/modules/hair_orders/ui/views/hair-order-view.tsx
+++ src/modules/hair_orders/ui/views/hair-order-view.tsx
@@ -24,12 +24,14 @@ import {
 	SimpleGrid,
 	Stack,
 	Text,
+	ThemeIcon,
 	Title,
 } from "@mantine/core";
 import { notifications } from "@mantine/notifications";
 import dayjs from "dayjs";
 import isoWeek from "dayjs/plugin/isoWeek";
 import { useSetAtom } from "jotai/index";
+import { ArrowDownRight, ArrowUpRight } from "lucide-react";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -157,6 +159,12 @@ function HairOrdersSuspense({ hairOrderId }: Props) {
 		(sum, transaction) => sum + transaction.amount,
 		0,
 	);
+
+	const hairTotalSoldFor =
+		hairAssignments.reduce(
+			(sum, hairAssignment) => sum + hairAssignment.soldFor,
+			0,
+		) / 100;
 
 	const transactionsCompletedTotal = transactions
 		.filter((transaction) => transaction.status === "COMPLETED")
@@ -339,6 +347,17 @@ function HairOrdersSuspense({ hairOrderId }: Props) {
 					</Paper>
 					<Paper withBorder p="md" radius="md" shadow="sm">
 						<Text size="lg" fw={700} ta="center">
+							Summary
+						</Text>
+						<Center>
+							<RecoveryCard
+								spent={transactionsTotal}
+								recovered={hairTotalSoldFor}
+							/>
+						</Center>
+					</Paper>
+					<Paper withBorder p="md" radius="md" shadow="sm">
+						<Text size="lg" fw={700} ta="center">
 							Transactions Summary
 						</Text>
 						<Center>
@@ -410,5 +429,56 @@ function HairOrdersSuspense({ hairOrderId }: Props) {
 				</Stack>
 			</GridCol>
 		</Grid>
+	);
+}
+
+interface RecoveryCardProps {
+	spent: number; // Example: -1000
+	recovered: number; // Example: 500
+}
+
+export function RecoveryCard({ spent, recovered }: RecoveryCardProps) {
+	const net = recovered + spent; // since spent is negative
+	const isProfit = net > 0;
+
+	return (
+		// <Card shadow="md" padding="lg" radius="md" withBorder>
+		<Stack gap="xs">
+			{/*<Text size="lg" fw={700}>*/}
+			{/*	Financial Recovery*/}
+			{/*</Text>*/}
+
+			<Group justify="space-between">
+				<Text>Spent</Text>
+				<Text c="red">{spent} £</Text>
+			</Group>
+
+			<Group justify="space-between">
+				<Text>Recovered</Text>
+				<Text c="blue">{recovered} £</Text>
+			</Group>
+
+			<Group justify="space-between">
+				<Group gap={4}>
+					<ThemeIcon
+						color={isProfit ? "green" : "red"}
+						variant="light"
+						size="sm"
+					>
+						{isProfit ? (
+							<ArrowUpRight size="1rem" />
+						) : (
+							<ArrowDownRight size="1rem" />
+						)}
+					</ThemeIcon>
+					<Text fw={500}>{isProfit ? "Profit" : "Still in debt"}</Text>
+				</Group>
+
+				<Text fw={700} c={isProfit ? "green" : "red"}>
+					{net} €
+				</Text>
+			</Group>
+		</Stack>
+		// </Card>
 	);
 }
