72e4033039e7643471cfba853c1aab2d1f9bf8eb
Martin <mselvenis@gmail.com>
2025-05-04 23:50:50 +0100

    feature: composable table

--- src/modules/ui/components/hair-used-table/hair-used-table-row-weight.tsx
+++ src/modules/ui/components/hair-used-table/hair-used-table-row-weight.tsx
@@ -1,11 +1,23 @@
 import { useHairUsedTableRowContext } from "@/modules/ui/components/hair-used-table/hair-used-table-row-context";
-import { Table, Text } from "@mantine/core";
+import { Group, Table, Text, Tooltip } from "@mantine/core";
+import { TriangleAlertIcon } from "lucide-react";
 
 function HairUsedTableRowWeight() {
 	const { hair } = useHairUsedTableRowContext();
 	return (
-		<Table.Td>
-			<Text>{hair.weightInGrams}g</Text>
+		<Table.Td
+			style={{
+				backgroundColor: hair.weightInGrams === 0 ? "#ffe6e6" : undefined, // light pink
+			}}
+		>
+			<Group>
+				<Text>{hair.weightInGrams}g</Text>
+				{hair.weightInGrams === 0 && (
+					<Tooltip label="Weight not assigned">
+						<TriangleAlertIcon size={16} color="red" />
+					</Tooltip>
+				)}
+			</Group>
 		</Table.Td>
 	);
 }
