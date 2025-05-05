79458c10ed2ca836a66265fdf9f01edcf62da32a
Martin <mselvenis@gmail.com>
2025-05-05 21:17:23 +0100

    fix: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
+import theme from "@/app/theme";
 import {
 	ColorSchemeScript,
-	DEFAULT_THEME,
 	MantineProvider,
-	createTheme,
-	mergeMantineTheme,
+	mantineHtmlProps,
 } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import Head from "next/head";
 import "./globals.css";
 import "@mantine/charts/styles.css";
 import "@mantine/dates/styles.css";
@@ -16,34 +13,14 @@ import "@mantine/notifications/styles.css";
 import { providerModals } from "@/lib/modal-helper";
 import { TRPCProvider } from "@/trpc/client";
 import { ModalsProvider } from "@mantine/modals";
+import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
 import type React from "react";
-import { breakpoints, colors } from "./theme";
-
-const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
-});
-const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
-});
 
 export const metadata: Metadata = {
 	title: "PRIVÉ Admin dashboard",
 	description: "PRIVÉ Admin dashboard",
 };
-
-const theme = mergeMantineTheme(
-	DEFAULT_THEME,
-	createTheme({
-		fontFamily: geistSans.style.fontFamily,
-		fontFamilyMonospace: geistMono.style.fontFamily,
-		breakpoints,
-		colors,
-	}),
-);
 
 export default function RootLayout({
 	children,
@@ -51,13 +28,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" suppressHydrationWarning>
-			<Head>
+		<html
+			lang="en"
+			{...mantineHtmlProps}
+			className={`${GeistSans.variable} ${GeistMono.variable}`}
+		>
+			<head>
 				<ColorSchemeScript />
-			</Head>
+			</head>
 			<body className="antialiased">
 				<TRPCProvider>
-					<MantineProvider theme={theme}>
+					<MantineProvider defaultColorScheme="dark" theme={theme}>
 						<ModalsProvider modals={providerModals}>
 							{children}
 							<Notifications />
