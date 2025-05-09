dff7f68967234e622be994ae120313f0006be3b0
Martin <mselvenis@gmail.com>
2025-05-09 12:49:03 +0100

    fix: add total field to hair order

---
+++ prisma/migrations/20250509114649_add_total_to_hair_order/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "hair_orders" ADD COLUMN     "total" INTEGER NOT NULL DEFAULT 0;
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,5 +1,15 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { cache } from "react";
+
 export const formatAmount = (amount: number) =>
 	new Intl.NumberFormat("en-UK", {
 		style: "currency",
 		currency: "GBP",
 	}).format(amount);
+
+export const getSession = cache(async () => {
+	return await auth.api.getSession({
+		headers: await headers(),
+	});
+});
