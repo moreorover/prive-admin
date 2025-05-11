a6a317472e400fc03953c37a21733c889cc7f97f
Martin <mselvenis@gmail.com>
2025-05-11 19:28:10 +0100

    feature: update customer view

--- src/modules/ui/components/stat-card.tsx
+++ src/modules/ui/components/stat-card.tsx
@@ -10,7 +10,7 @@ interface StatCardProps {
 
 export function StatCard({ title, value, icon }: StatCardProps) {
 	return (
-		<Paper withBorder p="md" radius="md" key={title}>
+		<Paper withBorder p="md" radius="md" shadow="sm">
 			<Group justify="space-between">
 				<Text size="xs" c="dimmed" className={classes.title}>
 					{title}
