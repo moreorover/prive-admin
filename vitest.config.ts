b5dbfd3aa84c77b17bd9fec184f31f509c483ece
Martin <mselvenis@gmail.com>
2025-05-13 15:17:51 +0100

    feature: update dashboard view

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,5 +9,11 @@ export default defineConfig({
 		globals: true,
 		environment: "jsdom",
 		setupFiles: "./vitest.setup.tsx",
+		coverage: {
+			// you can include other reporters, but 'json-summary' is required, json is recommended
+			reporter: ["text", "json-summary", "json"],
+			// If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+			reportOnFailure: true,
+		},
 	},
 });
