3a5d1a5b62b84a1abe1aeeaf25edabb7546b7f89
Martin <mselvenis@gmail.com>
2025-05-16 12:00:55 +0100

    feature: trpc

--- src/server/root.ts
+++ src/server/root.ts
@@ -1,10 +1,11 @@
-import { appointmentsRouter } from "@/modules/appointments/server/procedures";
-import { customersRouter } from "@/modules/customers/server/procedures";
-import { hairOrderRouter } from "@/modules/hair_orders/server/procedures";
-import { transactionsRouter } from "@/modules/transactions/server/procedures";
+import { appointmentsRouter } from "@/server/routers/appointments";
+import { customersRouter } from "@/server/routers/customers";
 import { dashboardRouter } from "@/server/routers/dashboard";
+import { hairOrderRouter } from "@/server/routers/hair-orders";
 import { hairAssignedRouter } from "@/server/routers/hairAssigned";
 import { notesRouter } from "@/server/routers/notes";
+import { transactionsRouter } from "@/server/routers/transactions";
+
 import { createCallerFactory, createTRPCRouter } from "@/server/trpc";
 
 export const appRouter = createTRPCRouter({
