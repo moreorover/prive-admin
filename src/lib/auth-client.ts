d66a6020bd2b1f494e5295b498d45aa123ed4fc9
martin <mselvenis@gmail.com>
2025-06-09 19:24:42 +0100

    feature: add admin plugin

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,6 @@
+import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
 	baseURL: process.env.NEXT_PUBLIC_APP_URL,
+	plugins: [adminClient()],
 });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import prisma from "@/lib/prisma";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
+import { admin } from "better-auth/plugins"
 
 export const auth = betterAuth({
 	database: prismaAdapter(prisma, {
@@ -11,5 +12,5 @@ export const auth = betterAuth({
 		enabled: true,
 		autoSignIn: false,
 	},
-	plugins: [nextCookies()],
+	plugins: [admin(), nextCookies()],
 });
