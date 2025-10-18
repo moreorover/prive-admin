ac398da8ff1fc3cc1b5a5154202d5eb376156286
Martin <mselvenis@gmail.com>
2025-10-18 21:23:39 +0300

    customers

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -9,8 +9,15 @@ export async function createContext({ context }: CreateContextOptions) {
   const session = await auth.api.getSession({
     headers: context.req.raw.headers,
   });
+
+  const ip =
+    context.req.header("CF-Connecting-IP") ||
+    context.req.header("x-forwarded-for") ||
+    context.req.header("x-real-ip");
+
   return {
     session,
+    ip,
   };
 }
 
