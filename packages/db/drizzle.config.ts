f837c93c04feadd7b58bf0d18422af7336e0f09c
Martin <mselvenis@gmail.com>
2026-01-02 17:35:20 +0200

    drizzle

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,11 +1,19 @@
+import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
-import { env } from "@prive-admin/env/server";
+
+dotenv.config({
+  path: "../../apps/server/.env",
+});
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL");
+}
 
 export default defineConfig({
   schema: "./src/schema",
   out: "./src/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
   },
 });
