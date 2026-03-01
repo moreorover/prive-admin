f571b8809b157d848b4084514cbbf3551596d881
Martynas Selvenis <martynas.selvenis@inventi.lt>
2026-03-01 12:54:27 +0200

    chore: remove all files for tanstack rewrite

--- src/modules/appointments/types.ts
+++
@@ -1,8 +0,0 @@
-import type { AppRouter } from "@/server/root";
-import type { inferRouterOutputs } from "@trpc/server";
-
-export type GetPersonnelOptions =
-	inferRouterOutputs<AppRouter>["customers"]["getPersonnelByAppointmentId"];
-
-export type GetAppointments =
-	inferRouterOutputs<AppRouter>["appointments"]["getAppointmentsBetweenDates"];
