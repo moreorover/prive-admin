4341240b6c25d56143dc91de080dd9c7540197ee
Martin <mselvenis@gmail.com>
2026-05-10 18:34:38 +0300

    chore(web): redirect /salons to /legal-entities

--- apps/web/src/routes/_authenticated/salons/index.tsx
+++ apps/web/src/routes/_authenticated/salons/index.tsx
@@ -1,30 +1,7 @@
-import { Button, Card, Container, Group, Stack, Title } from "@mantine/core"
-import { useQuery } from "@tanstack/react-query"
-import { Link, createFileRoute } from "@tanstack/react-router"
-
-import { SalonsTable } from "@/components/salons-table"
-import { listSalons } from "@/functions/salons"
+import { createFileRoute, redirect } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/_authenticated/salons/")({
-  component: SalonsIndex,
+  beforeLoad: () => {
+    throw redirect({ to: "/legal-entities" })
+  },
 })
-
-function SalonsIndex() {
-  const q = useQuery({ queryKey: ["salons"], queryFn: () => listSalons() })
-
-  return (
-    <Container size="lg">
-      <Stack p="md">
-        <Group justify="space-between">
-          <Title order={3}>Salons</Title>
-          <Button renderRoot={(props) => <Link to="/salons/$salonId" params={{ salonId: "new" }} {...props} />}>
-            New salon
-          </Button>
-        </Group>
-        <Card withBorder>
-          <SalonsTable salons={q.data ?? []} />
-        </Card>
-      </Stack>
-    </Container>
-  )
-}
