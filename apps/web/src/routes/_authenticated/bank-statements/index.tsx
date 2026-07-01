26ae4c752d6e65c117ed0e2cfc86f87f9b819f91
Martynas <mselvenis@gmail.com>
2026-07-01 20:40:17 +0100

    Remove legacy redirect routes (#205)

--- apps/web/src/routes/_authenticated/bank-statements/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router"
-
-export const Route = createFileRoute("/_authenticated/bank-statements/")({
-  beforeLoad: () => {
-    throw redirect({ to: "/legal-entities" })
-  },
-})
