3aadd6a2d739840c1b3db53cce20d92114e4b446
Martin <mselvenis@gmail.com>
2026-08-02 00:06:26 +0300

    fix: support cloudflare worker api origin

--- apps/web/src/utils/trpc.ts
+++ apps/web/src/utils/trpc.ts
@@ -16,7 +16,7 @@ export const queryClient = new QueryClient({
 const trpcClient = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "/trpc",
+      url: `${import.meta.env.VITE_SERVER_URL ?? ""}/trpc`,
       fetch(url, options) {
         return fetch(url, {
           ...options,
