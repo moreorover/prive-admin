0ed56c1de6e05c2c6095db6b3c1392465abb27f3
Martynas <mselvenis@gmail.com>
2026-08-22 19:39:37 +0100

    chore(deps): configure taze updates (#298)

---
+++ taze.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "taze"
+
+export default defineConfig({
+  githubActions: {
+    style: "auto",
+  },
+  fastNpmMetaApiEndpoint: "https://npm.antfu.dev/",
+  includeLocked: true,
+  maturityPeriod: 7,
+  maturityPeriodExclude: ["@mantine/*", "better-auth"],
+})
